bb674b6fa815ff7b1eb8d55e616ee315138121b3
RubabaBinteMahbub <rubababintemahbub2007@gmail.com>
2025-11-10 00:12:58 +0600

    Add deletion functionality for array elements
    Implement a program to delete an element from an array.

---
+++ deletion.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){
+   int size,pos;
+   printf("Enter size = ");
+   scanf("%d",&size);
+   int a[size],i;
+   printf("Enter elements :");
+   for(i=0;i<size;i++){
+      scanf("%d",&a[i]);
+   }
+   
+   printf("Enter the position = ");
+   scanf("%d",&pos);
+   
+   for(i=pos-1;i<=size-1;i++){
+      a[i]=a[i+1];
+   }
+  
+   size--;
+   
+   printf("Updated array : ");
+   for(i=0;i<size;i++){
+       printf(" %d",a[i]);
+   }
+    
+   return 0;
+}
